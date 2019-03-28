0a96108ae257a12c1dd5909d3ce40fd1c237d40b
brandonhardesty <brandonhardesty@172.24.1.243>
2019-03-28 13:06:26 -0400

    Added csv files and modified readData function.

--- Basketball/src/Main.java
+++ Basketball/src/Main.java
@@ -7,19 +7,26 @@ public class Main {
 	public ArrayList<Player> readData(File file) {
 		ArrayList<Player> players = new ArrayList<Player>();
 		ArrayList<String> lines = new ArrayList<String>();
+		String pos = "";
+		if(file.getName().equals("F.csv")) {
+			pos = "F";
+		}else {
+			pos = "G";
+		}
 		try {
 			Scanner reader = new Scanner(file);
 			while(reader.hasNextLine()) {
 				lines.add(reader.nextLine());
 			}
-			String[] parts = new String[5];
+			String[] parts = new String[4];
 			double[] stats = {0,0,0,0};
 			for(String line:lines) {
-				parts = line.split(";");
+				parts = line.split(",");
 				for(int i = 0; i<stats.length;i++) {
 					stats[i] = Double.parseDouble(parts[i]);
 				}
-				players.add(new Player(stats,parts[4]));
+				
+				players.add(new Player(stats,pos));
 			}
 			
 		} catch (FileNotFoundException e) {
