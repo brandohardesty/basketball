c393395386109744b3df038246bb5120ec78a225
Audrey Bunn <37702798+acb4905@users.noreply.github.com>
2019-04-09 13:46:13 -0400

    Update ClassificationTests.java
    Just needs K's test case in the 3rd try catch

--- Basketball/src/ClassificationTests.java
+++ Basketball/src/ClassificationTests.java
@@ -9,14 +9,45 @@ public class ClassificationTests extends TestCase {
 	ArrayList<double[]> forwardStats= new ArrayList<double[]>();
 	ArrayList<double[]> guardStats= new ArrayList<double[]>();
 	BruteForce bf = new BruteForce();
+	LVQ model;
 	//LVQ lvq = new LVQ();
 	ArrayList<String[]> analysis = new ArrayList <String[]> ();
 	
 	ClassificationTests(){
 		File g = new File("G.csv"); //$NON-NLS-1$
 		File f = new File("F.csv"); //$NON-NLS-1$
+		double[] gStats = {22,5,.8,.3};
+		double[] fStats = {20,10,.7,.2};
+		Player guardCodeBook = new Player(gStats,"G");
+		Player fowardCodeBook = new Player(fStats,"F");
+		ArrayList<Player> cbv = new ArrayList<Player>();
+		cbv.add(guardCodeBook);
+		cbv.add(fowardCodeBook);
 		ArrayList<Player> guardData = Main.readData(g);
 		ArrayList<Player> fowardData = Main.readData(f);
+		ArrayList<Player> allData = new ArrayList<Player>();
+		allData.addAll(guardData);
+		allData.addAll(fowardData);
+		double[] pGStats= new double[] {22.88, 5.175, 0.8255, 0.3558};
+		double[] pFStats= new double[] {21.72, 12, 0.74365, 0.23925};
+		double[] mFStats= new double[] {22.2, 9, 0.9, 0.3};
+		double[] mGStats= new double[] {22.2, 8, 0.9, 0.3};
+
+
+		Player pureGuard = new Player(pGStats,"G");
+		Player pureFoward = new Player(pFStats,"F");
+		Player comboFoward = new Player(mFStats,"F");
+		Player comboGuard = new Player(mGStats,"G");
+		ArrayList<Player> testData = new ArrayList<Player>();
+		testData.add(pureGuard);
+		testData.add(pureFoward);
+		testData.add(comboFoward);
+		testData.add(comboGuard);
+		
+		
+		
+		model = new LVQ(allData,cbv,testData);
+		model.train(.3,2000);
 		
 		for (int i=0; i<20; i++) {
 			forwardStats.add(fowardData.get(i).getStats());
@@ -28,12 +59,29 @@ public class ClassificationTests extends TestCase {
 	public void PureGuardShouldBeGuard() {
 		double[] inputStats= new double[] {22.88, 5.175, 0.8255, 0.3558};
 		String summary[] = new String[2];
+		
 		try {
 			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
 			summary[0]="Pass";
 		}
 		catch(Exception e) {
 			summary[0]= "Fail";
+		}
+		
+		try {
+			assertEquals("G", model.classify(model.getTestData.get(0)));
+			summary[1]="Pass";
+		}
+		catch(Exception e) {
+			summary[1]= "Fail";
+		}
+		
+		try {
+			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			summary[2]="Pass";
+		}
+		catch(Exception e) {
+			summary[2]= "Fail";
 		}
 		//Repeat try catch blocks for lvq and k's with the same test data
 	}
@@ -41,12 +89,29 @@ public class ClassificationTests extends TestCase {
 	public void PureForwardShouldBeForward() {
 		double[] inputStats= new double[] {21.72, 12, 0.74365, 0.23925};
 		String summary[] = new String[2];
+		
 		try {
 			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
 			summary[0]="Pass";
 		}
 		catch(Exception e) {
 			summary[0]= "Fail";
+		}
+		
+		try {
+			assertEquals("F", model.classify(model.getTestData.get(1)));
+			summary[1]="Pass";
+		}
+		catch(Exception e) {
+			summary[1]= "Fail";
+		}
+		
+		try {
+			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			summary[2]="Pass";
+		}
+		catch(Exception e) {
+			summary[2]= "Fail";
 		}
 		//Repeat try catch blocks for lvq and k's with the same test data
 	}
@@ -54,12 +119,29 @@ public class ClassificationTests extends TestCase {
 	public void MixedForwardDominantStatsShouldBeForward() {
 		double[] inputStats= new double[] {22.2, 9, 0.9, 0.3};
 		String summary[] = new String[2];
+		
 		try {
 			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
 			summary[0]="Pass";
 		}
 		catch(Exception e) {
 			summary[0]= "Fail";
+		}
+		
+		try {
+			assertEquals("F", model.classify(model.getTestData.get(2)));
+			summary[1]="Pass";
+		}
+		catch(Exception e) {
+			summary[1]= "Fail";
+		}
+		
+		try {
+			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			summary[2]="Pass";
+		}
+		catch(Exception e) {
+			summary[2]= "Fail";
 		}
 		//Repeat try catch blocks for lvq and k's with the same test data
 	}
@@ -67,12 +149,29 @@ public class ClassificationTests extends TestCase {
 	public void MixedGuardDominantStatsShouldBeGuard() {
 		double[] inputStats= new double[] {22.2, 8, 0.9, 0.3};
 		String summary[] = new String[2];
+		
 		try {
 			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
 			summary[0]="Pass";
 		}
 		catch(Exception e) {
 			summary[0]= "Fail";
+		}
+		
+		try {
+			assertEquals("G", model.classify(model.getTestData.get(3)));
+			summary[1]="Pass";
+		}
+		catch(Exception e) {
+			summary[1]= "Fail";
+		}
+		
+		try {
+			assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			summary[2]="Pass";
+		}
+		catch(Exception e) {
+			summary[2]= "Fail";
 		}
 		//Repeat try catch blocks for lvq and k's with the same test data
 	}
