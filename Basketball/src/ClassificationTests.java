070a864e6912e92c5a490f365205c668c0b799a6
Audrey Bunn <37702798+acb4905@users.noreply.github.com>
2019-04-13 17:20:56 -0400

    Update ClassificationTests.java
    Added NCAA data tests with fail percentage and minimum and maximum run time being recorded into a new file
    
    ** NEEDS LVQ AND K's RUN PROMPTS

--- Basketball/src/ClassificationTests.java
+++ Basketball/src/ClassificationTests.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.lang.AssertionError;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import junit.framework.TestCase;
 public class ClassificationTests extends TestCase {
 	ArrayList<double[]> forwardStats= new ArrayList<double[]>();
 	ArrayList<double[]> guardStats= new ArrayList<double[]>();
+	ArrayList<double[]> forwardTestStats= new ArrayList<double[]>();
+	ArrayList<double[]> guardTestStats= new ArrayList<double[]>();
 	BruteForce bf = new BruteForce();
 	LVQ model;
 	//LVQ lvq = new LVQ();
@@ -16,6 +19,8 @@ public class ClassificationTests extends TestCase {
 	ClassificationTests(){
 		File g = new File("G.csv"); //$NON-NLS-1$
 		File f = new File("F.csv"); //$NON-NLS-1$
+		File gTest = new File("GTestData.csv"); //$NON-NLS-1$
+		File fTest = new File("FTestData.csv");
 		double[] gStats = {22,5,.8,.3};
 		double[] fStats = {20,10,.7,.2};
 		Player guardCodeBook = new Player(gStats,"G");
@@ -25,6 +30,8 @@ public class ClassificationTests extends TestCase {
 		cbv.add(fowardCodeBook);
 		ArrayList<Player> guardData = Main.readData(g);
 		ArrayList<Player> fowardData = Main.readData(f);
+		ArrayList<Player> guardTestData = Main.readData(gTest);
+		ArrayList<Player> fowardTestData = Main.readData(fTest);
 		ArrayList<Player> allData = new ArrayList<Player>();
 		allData.addAll(guardData);
 		allData.addAll(fowardData);
@@ -52,6 +59,11 @@ public class ClassificationTests extends TestCase {
 		for (int i=0; i<20; i++) {
 			forwardStats.add(fowardData.get(i).getStats());
 			guardStats.add(guardData.get(i).getStats());
+		}
+		
+		for (int i=0; i<10; i++) {
+			forwardTestStats.add(fowardTestData.get(i).getStats());
+			guardTestStats.add(guardTestData.get(i).getStats());
 		}
 	}
 	
@@ -174,5 +186,184 @@ public class ClassificationTests extends TestCase {
 			summary[2]= "Fail";
 		}
 		//Repeat try catch blocks for lvq and k's with the same test data
+	}
+	
+	public void NCAAStatTestAnalysis() {
+		double[] inputStats= new double[3];
+		int bfGuardFailCount=0;
+		int lvqGuardFailCount=0;
+		int kGuardFailCount=0;
+		
+		int bfForwardFailCount=0;
+		int lvqForwardFailCount=0;
+		int kForwardFailCount=0;
+		
+		long bfMaxTime=0;
+		long bfMinTime=100000;
+		long lvqMaxTime=0;
+		long lvqMinTime=100000;
+		long kMaxtime=0;
+		long kMinTime=100000;
+		
+		//for all 10 ncaa forwards
+		for (stats in forwardTestStats) {
+			inputStats=stats;
+			
+			//Get Run Times
+			long bfStartTime = System.nanoTime();
+			bf.bruteForceClassification(forwardStats, guardStats, inputStats);
+			long bfEndTime = System.nanoTime();
+			
+			if (((bfEndTime-bfStartTime)/1000000)<bfMinTime) {
+				bfMinTime=(bfEndTime-bfStartTime)/1000000;
+			}
+			
+			if (((bfEndTime-bfStartTime)/1000000)>bfMaxTime) {
+				bfMaxTime=(bfEndTime-bfStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("Forward", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			}
+			catch(Exception e) {
+				bfForwardFailCount+=1;
+			}
+			
+			//Get Run Times
+			long lvqStartTime = System.nanoTime();
+			//run with current input data
+			model.classify(model.getTestData.get(3));
+			long lvqEndTime = System.nanoTime();
+			
+			if (((lvqEndTime-lvqStartTime)/1000000)<lvqMinTime) {
+				lvqMinTime=(lvqEndTime-lvqStartTime)/1000000;
+			}
+			
+			if (((lvqEndTime-lvqStartTime)/1000000)>lvqMaxTime) {
+				lvqMaxTime=(lvqEndTime-lvqStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("F", model.classify(model.getTestData.get(3)));
+			}
+			catch(Exception e) {
+				lvqForwardFailCount+=1;
+			}
+			
+			//Get Run Times
+			long kStartTime = System.nanoTime();
+			//run k with current input data
+			bf.bruteForceClassification(forwardStats, guardStats, inputStats);
+			long kEndTime = System.nanoTime();
+			
+			if (((kEndTime-kStartTime)/1000000)<kMinTime) {
+				kMinTime=(kEndTime-kStartTime)/1000000;
+			}
+			
+			if (((kEndTime-kStartTime)/1000000)>kMaxTime) {
+				kMaxTime=(kEndTime-kStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("Forward", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			}
+			catch(Exception e) {
+				kForwardFailCount!=1;
+			}
+		}
+		
+		//for all 10 ncaa guards
+		for (stats in guardTestStats) {
+			inputStats=stats;
+			
+			//Get Run Times
+			long bfStartTime = System.nanoTime();
+			bf.bruteForceClassification(forwardStats, guardStats, inputStats);
+			long bfEndTime = System.nanoTime();
+			
+			if (((bfEndTime-bfStartTime)/1000000)<bfMinTime) {
+				bfMinTime=(bfEndTime-bfStartTime)/1000000;
+			}
+			
+			if (((bfEndTime-bfStartTime)/1000000)>bfMaxTime) {
+				bfMaxTime=(bfEndTime-bfStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			}
+			catch(Exception e) {
+				bfGuardFailCount+=1;
+			}
+			
+			//Get Run Times
+			long lvqStartTime = System.nanoTime();
+			//run with current input data
+			model.classify(model.getTestData.get(3));
+			long lvqEndTime = System.nanoTime();
+			
+			if (((lvqEndTime-lvqStartTime)/1000000)<lvqMinTime) {
+				lvqMinTime=(lvqEndTime-lvqStartTime)/1000000;
+			}
+			
+			if (((lvqEndTime-lvqStartTime)/1000000)>lvqMaxTime) {
+				lvqMaxTime=(lvqEndTime-lvqStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("G", model.classify(model.getTestData.get(3)));
+			}
+			catch(Exception e) {
+				lvqGuardFailCount+=1;
+			}
+			
+			//Get Run Times
+			long kStartTime = System.nanoTime();
+			//run k with current input data
+			bf.bruteForceClassification(forwardStats, guardStats, inputStats);
+			long kEndTime = System.nanoTime();
+			
+			if (((kEndTime-kStartTime)/1000000)<kMinTime) {
+				kMinTime=(kEndTime-kStartTime)/1000000;
+			}
+			
+			if (((kEndTime-kStartTime)/1000000)>kMaxTime) {
+				kMaxTime=(kEndTime-kStartTime)/1000000;
+			}
+			
+			//Check for accuracy
+			try {
+				assertEquals("Guard", bf.bruteForceClassification(forwardStats, guardStats, inputStats));
+			}
+			catch(Exception e) {
+				kGuardFailCount!=1;
+			}
+		}
+		
+		throws IOException {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(ncaaTestResults));
+			writer.write("Brute Force: \n "
+					+ "Was incorrect " + bfGuardFailCount*10 + "% of the time when classifying guards \n "
+					+ "Was incorrect "+ bfForwardFailCount*10 + "% of the time when classifying forwards \n"
+					+ "Had a maximum run time of " + bfMaxTime + " milliseconds"
+					+ "Had a minimum run time of " + bfMinTime + " milliseconds \n \n");   
+			writer.write("Linear Vector Quantization: \n "
+					+ "Was incorrect " + lvqGuardFailCount*10 + "% of the time when classifying guards \n "
+					+ "Was incorrect "+ lvqForwardFailCount*10 + "% of the time when classifying forwards \n"
+					+ "Had a maximum run time of " + lvqMaxTime + " milliseconds"
+					+ "Had a minimum run time of " + lvqMinTime + " milliseconds \n \n");   
+			writer.write("K's Nearest Neighbors: \n "
+					+ "Was incorrect " + kGuardFailCount*10 + "% of the time when classifying guards \n "
+					+ "Was incorrect "+ kForwardFailCount*10 + "% of the time when classifying forwards \n"
+					+ "Had a maximum run time of " + kMaxTime + " milliseconds"
+					+ "Had a minimum run time of " + kMinTime + " milliseconds");   
+			writer.close();
+		}
+		
 	}
 }
